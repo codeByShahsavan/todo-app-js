777ddacabd17975f4d25fa07bda8dbb242b4923f
fatemeh shahsavan <shahsavan051@yahoo.com>
2025-08-12 10:18:48 +0330

    show message

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
 const  taskInput=document.getElementById("task-input")
 const  dateInput=document.getElementById("date-input")
 const  addButton=document.getElementById("add-button")
+const  alertMessage=document.getElementById("alert-message")
+
 
 const todos=[]
+
+const showAlert=(message,type)=>{
+    alertMessage.innerHTML=""
+    const alert=document.createElement("p")
+    alert.innerText=message
+    alert.classList.add("alert")
+    alert.classList.add(`alert-${type}`)
+    alertMessage.append(alert)
+    setTimeout(()=>{
+        alert.style.display="none"
+    },2000)
+}
+
 
 const addHandler=()=>{
     const task=taskInput.value
@@ -17,9 +32,10 @@ const addHandler=()=>{
       taskInput.value=""
       dateInput.value=""
       console.log(todos)
+      showAlert("Todo added successfully","success")
     }
     else{
-        alert("warning")
+        showAlert("Please enter a todo!","error")
     }
 }
 addButton.addEventListener("click",addHandler)
