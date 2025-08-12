44dd3bb5594e8526c1d733349986d3a9c5ed5ce6
fatemeh shahsavan <shahsavan051@yahoo.com>
2025-08-12 12:48:36 +0330

    show todo dynamic

--- js/app.js
+++ js/app.js
@@ -2,10 +2,14 @@ const  taskInput=document.getElementById("task-input")
 const  dateInput=document.getElementById("date-input")
 const  addButton=document.getElementById("add-button")
 const  alertMessage=document.getElementById("alert-message")
-
+    const todosBody=document.querySelector("tbody")
 
 const todos=JSON.parse(localStorage.getItem("todos"))||[]
 
+const saveToLocalStorage=()=>{
+localStorage.setItem("todos",JSON.stringify(todos))
+
+}
 const generateId=()=>{
      return Math.round(Math.random()*Math.random()*Math.pow(10,15)).toString()
 
@@ -25,12 +29,32 @@ const showAlert=(message,type)=>{
     },2000)
 }
 
-const saveToLocalStorage=()=>{
-localStorage.setItem("todos",JSON.stringify(todos))
 
-}
+ const displayTodos=()=>{
+    todosBody.innerHTML=""
+  if(!todos.length){
+    todosBody.innerHTML="<tr><td colspan='4'>No task found!</td></tr>"
+    return;
+  }
+  todos.forEach(todo => {
 
+    todosBody.innerHTML+=
+    `<tr>
+    <td>${todo.task}</td>
+    <td>${todo.date||"No Date"}</td>
+    <td>${todo.completed?"Completed":"Pending"}</td>
+    <td>
+    <button>Edit</button>
+    <button>Do</button>
+    <button>Delete</button>
 
+    </td>
+    
+    </tr>
+    `
+  });
+   
+ }
 const addHandler=()=>{
     const task=taskInput.value
     const date=dateInput.value
@@ -45,15 +69,20 @@ const addHandler=()=>{
     if(task){
       todos.push(todo)
       saveToLocalStorage()
+     displayTodos()
+ 
+
       taskInput.value=""
       dateInput.value=""
       console.log(todos)
       showAlert("Todo added successfully","success")
-    
 
     }
     else{
         showAlert("Please enter a todo!","error")
     }
 }
+
+window.addEventListener("load",displayTodos)
 addButton.addEventListener("click",addHandler)
+
